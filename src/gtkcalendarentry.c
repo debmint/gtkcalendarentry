2e1792429f9527703c7e56586f96f942abd53386
debmint <debmint49@gmail.com>
2017-07-31 09:42:27 -0500

    Added setup instructions to README file and added copyright message to source files.

--- src/gtkcalendarentry.c
+++ src/gtkcalendarentry.c
@@ -1,3 +1,23 @@
+/*
+Copyright (c) 2017 David Breeding
+
+This file is part of gtkcalendarentry.
+
+gtkcalendarentry is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+gtkcalendarentry is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with gtkcalendarentry (see the file "COPYING").  If not,
+see <http://www.gnu.org/licenses/>.
+*/
+
 /* ************************************************************************ *
  * gtkcalendarentry.c - Source to generate GtkCalendarEntry widget          $
  * $Id::                                                                    $
